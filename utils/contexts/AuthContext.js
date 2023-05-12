8a3f85b71f947f9301e470b98b252433d1e85c5c
GAURAV <iamgaurav201@gmail.com>
2023-05-12 23:08:18 +0530

    new commit

--- utils/contexts/AuthContext.js
+++ utils/contexts/AuthContext.js
@@ -1,13 +1,13 @@
-// import { createContext, useContext } from "react";
-// import useFirebaseAuth from "../firebase/useFirebaseAuth";
+import { createContext, useContext } from "react";
+import useFirebaseAuth from "../firebase/useFirebaseAuth";
 
-// const AuthContext = createContext({
-//   currentUser: null,
-//   loading: true,
-// });
+const AuthContext = createContext({
+  currentUser: null,
+  loading: true,
+});
 
-// export function AuthUserProvider({ children }) {
-//   const auth = useFirebaseAuth();
-//   return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
-// }
-// export const useAuth = () => useContext(AuthContext);
+export function AuthUserProvider({ children }) {
+  const auth = useFirebaseAuth();
+  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
+}
+export const useAuth = () => useContext(AuthContext);
