f007c1d5296b171c156b50ce12f0efb263c5bbc3
GAURAV <iamgaurav201@gmail.com>
2023-05-31 14:04:50 +0530

    new commit

--- pages/paymentFailure/index.js
+++ pages/paymentFailure/index.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import styles from "../../styles/paymentfailure.module.css";
 
 const PaymentFailure = () => {
   return (
-    <div>PaymentFailure</div>
+    <div>
+      <div className={styles.container}>
+  <div className={`${styles.ui} ${styles.middle} ${styles.aligned} ${styles.center} ${styles.aligned} ${styles.grid}`}>
+    <div className={`${styles.ui} ${styles.eight} ${styles.wide} ${styles.column}`}>
+   
+      <form className={`${styles.ui} ${styles.large} ${styles.form}`}>
+                
+          <div className={`${styles.ui} ${styles.icon} ${styles.negative} ${styles.message}`}>
+            <i className={`${styles.warning} ${styles.icon}`}></i>
+            <div className={styles.content}>
+              <div className={styles.header}>
+                Oops! Something went wrong.
+              </div>
+              <p>While trying to reserve money from your account</p>
+            </div>
+            
+         </div>
+      
+          <span className={`${styles.ui} ${styles.large} ${styles.teal} ${styles.submit} ${styles.fluid} ${styles.button}`}>Try again</span>
+      
+      </form>
+    </div>
+  </div>
+</div>
+    </div>
   )
 }
 
--- pages/paymentSuccess/index.js
+++ pages/paymentSuccess/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import styles from "../../styles/paymentsuccess.module.css";
 
 const PaymentScuccess = () => {
   return (
-    <div>PaymentScuccess</div>
+    <div className={styles.paymentSuccess}>
+    <div className={styles.card}>
+    <div style={{borderRadius:"200px", height:"200px", width:"200px", background: "#F8FAF5" ,margin:"0 auto"}}>
+      <i className={styles.checkmark}>✓</i>
+    </div>
+      <h1 className={styles.heading}>Success</h1> 
+      <p className={styles.para}>We received your purchase request;<br/> we'll be in touch shortly!</p>
+    </div>
+    </div>
   )
 }
 
