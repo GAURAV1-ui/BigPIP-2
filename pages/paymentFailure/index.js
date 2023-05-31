6c35824c658aa4d2810b402e0a76bc96d4c716a0
GAURAV <iamgaurav201@gmail.com>
2023-05-31 14:41:23 +0530

    new commit

--- pages/paymentFailure/index.js
+++ pages/paymentFailure/index.js
@@ -3,31 +3,15 @@ import styles from "../../styles/paymentfailure.module.css";
 
 const PaymentFailure = () => {
   return (
-    <div>
-      <div className={styles.container}>
-  <div className={`${styles.ui} ${styles.middle} ${styles.aligned} ${styles.center} ${styles.aligned} ${styles.grid}`}>
-    <div className={`${styles.ui} ${styles.eight} ${styles.wide} ${styles.column}`}>
-   
-      <form className={`${styles.ui} ${styles.large} ${styles.form}`}>
-                
-          <div className={`${styles.ui} ${styles.icon} ${styles.negative} ${styles.message}`}>
-            <i className={`${styles.warning} ${styles.icon}`}></i>
-            <div className={styles.content}>
+    <div className={styles.failure}>
+
               <div className={styles.header}>
-                Oops! Something went wrong.
-              </div>
+                <h3>Oops! Something went wrong.</h3>
               <p>While trying to reserve money from your account</p>
-            </div>
-            
-         </div>
-      
-          <span className={`${styles.ui} ${styles.large} ${styles.teal} ${styles.submit} ${styles.fluid} ${styles.button}`}>Try again</span>
-      
-      </form>
-    </div>
-  </div>
+              </div>
+          <button className={styles.fButton}>Try again</button>
+
 </div>
-    </div>
   )
 }
 
